dea445ed705eadb9be3e1b551c8901b703e8e52f
11BelowStudio <r_m_lowe@outlook.com>
2021-05-29 23:27:10 +0100

    this stuff wasn't committed
    so I'm committing it now

---
+++ Assets/Scripts/Game/Fighter/ActionInput.cs
@@ -0,0 +1,13 @@
+﻿namespace Game.Fighter
+{
+    public enum ActionInput
+    {
+        JUMP,
+        SHORT_NEUTRAL,
+        SHORT_DIAGONAL,
+        MID_NEUTRAL,
+        MID_DIAGONAL,
+        PROJECTILE_NEUTRAL,
+        PROJECTILE_DIAGONAL
+    }
+}
---
+++ Assets/Scripts/Game/Fighter/IGiveInputs.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Game.Fighter
+{
+    /// <summary>
+    /// Gives inputs to the fighter
+    /// </summary>
+    public interface IGiveInputs
+    {
+
+        /// <summary>
+        /// obtains the movement input from the left stick
+        /// </summary>
+        /// <returns>current movement input</returns>
+        public Vector2 GetMovementInput();
+
+        /// <summary>
+        /// checks to see if the Any button was pressed
+        /// </summary>
+        /// <returns>true if the player pressed the Any button</returns>
+        public bool IsTheAnyButtonPressed();
+
+        /// <summary>
+        /// Returns what the current input from the right stick is
+        /// </summary>
+        /// <returns>right stick action</returns>
+        public ActionInput GetRightStickAction();
+    }
+}
