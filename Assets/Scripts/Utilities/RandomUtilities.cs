15533056847983e501576b57b8c1c463fe8a374b
11BelowStudio <r_m_lowe@outlook.com>
2021-06-05 02:30:34 +0100

    pls to commit

--- Assets/Scripts/Utilities/RandomUtilities.cs
+++ Assets/Scripts/Utilities/RandomUtilities.cs
@@ -25,5 +25,47 @@ namespace Utilities
             return enumerable.ElementAt(Random.Range(0, enumerable.Count()));
 
         }
+        
+        /// <summary>
+        /// Shuffles this IList
+        /// </summary>
+        /// <param name="list"></param>
+        /// <typeparam name="T"></typeparam>
+        public static void Shuffle<T>(this IList<T> list)
+        {
+
+            var size = list.Count;
+
+            for (var i = 0; i < size; i++)
+            {
+                list.Swap(i, Random.Range(i, size));
+            }
+        }
+
+        /// <summary>
+        /// Creates and shuffles a copy of the given list
+        /// </summary>
+        /// <param name="theList"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static IList<T> ShuffleCopy<T>(this IList<T> theList)
+        {
+            var newList = new List<T>(theList);
+            Shuffle(newList);
+            return newList;
+        }
+ 
+        /// <summary>
+        /// Swaps the items at the given indexes of this list.
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        /// <typeparam name="T"></typeparam>
+        public static void Swap<T>(this IList<T> list, int i, int j) {
+            var temp = list[i];
+            list[i] = list[j];
+            list[j] = temp;
+        }
     }
 }
